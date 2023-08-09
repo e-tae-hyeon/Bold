bc978a490d6fea15347de49cf636f3ff59551344
LTH <johtta888@naver.com>
2023-08-09 19:08:33 +0900

    Feat: 추가 today indicator in home

--- src/components/Home/template/TodayIndicator/index.tsx
+++ src/components/Home/template/TodayIndicator/index.tsx
@@ -1,11 +1,13 @@
 import AppText from 'components/@base/AppText';
+import {format} from 'date-fns';
+import {ko} from 'date-fns/locale';
 import React from 'react';
 import {View} from 'react-native';
 
 function TodayIndicator() {
   return (
-    <View className="items-center justify-center p-4">
-      <AppText>today</AppText>
+    <View className="items-center justify-center p-8">
+      <AppText>{format(new Date(), 'yy년 M월 d일 (E)', {locale: ko})}</AppText>
     </View>
   );
 }
