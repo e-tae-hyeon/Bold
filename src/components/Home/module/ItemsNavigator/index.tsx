13029a1239fd3ac91182f53cd81fa70b825b11a7
LTH <johtta888@naver.com>
2023-08-10 21:20:15 +0900

    Feat: 수정 recent item list 클릭 시, items screen 으로 이동

--- src/components/Home/module/ItemsNavigator/index.tsx
+++ src/components/Home/module/ItemsNavigator/index.tsx
@@ -1,34 +1,20 @@
-import {useNavigation} from '@react-navigation/native';
 import colors from 'common/styles/colors';
 import AppText from 'components/@base/AppText';
 import FlexView from 'components/@base/FlexView';
 import SvgIcon from 'components/@base/SvgIcon';
-import {RootStackNavigationProps} from 'navigations/RootStack/types';
 import React from 'react';
-import {Pressable, View} from 'react-native';
-import {formatDateString} from 'utils/formatDate';
 
 function ItemsNavigator() {
-  const {navigate} = useNavigation<RootStackNavigationProps>();
-
-  const handlePress = () => {
-    navigate('Items', {dateString: formatDateString(new Date())});
-  };
-
   return (
-    <View className="items-center justify-center">
-      <Pressable onPress={handlePress} className="p-4">
-        <FlexView
-          direction="row"
-          gapSize="tiny"
-          className="items-center justify-center">
-          <AppText typoStyle="B2" color={colors.gray[300]}>
-            모두 보기
-          </AppText>
-          <SvgIcon name="chevronRight" size={16} color={colors.gray[300]} />
-        </FlexView>
-      </Pressable>
-    </View>
+    <FlexView
+      direction="row"
+      gapSize="tiny"
+      className="items-center justify-center p-2">
+      <AppText typoStyle="B2" color={colors.gray[300]}>
+        모두 보기
+      </AppText>
+      <SvgIcon name="chevronRight" size={16} color={colors.gray[300]} />
+    </FlexView>
   );
 }
 
--- src/components/Home/template/RecentItemList/index.tsx
+++ src/components/Home/template/RecentItemList/index.tsx
@@ -1,15 +1,22 @@
-import {useFocusEffect} from '@react-navigation/native';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import FlexView from 'components/@base/FlexView';
 import ItemsNavigator from 'components/Home/module/ItemsNavigator';
 import ItemCard from 'components/Item/module/ItemCard';
 import useItems from 'hooks/useItems';
+import {RootStackNavigationProps} from 'navigations/RootStack/types';
 import React, {useCallback} from 'react';
+import {Pressable} from 'react-native';
 import useHomeStore from 'stores/useHomeStore';
 import {formatDateString} from 'utils/formatDate';
 
 function RecentItemList() {
   const {items, initItems} = useHomeStore();
   const {getItems} = useItems();
+  const {navigate} = useNavigation<RootStackNavigationProps>();
+
+  const handlePress = () => {
+    navigate('Items', {dateString: formatDateString(new Date())});
+  };
 
   useFocusEffect(
     useCallback(() => {
@@ -22,12 +29,14 @@ function RecentItemList() {
   );
 
   return (
-    <FlexView gapSize="small" className="px-8 py-4">
-      {items.slice(0, 3).map(item => (
-        <ItemCard item={item} key={item.id} isClipContent />
-      ))}
-      <ItemsNavigator />
-    </FlexView>
+    <Pressable onPress={handlePress}>
+      <FlexView gapSize="small" className="px-8 py-4">
+        {items.slice(0, 3).map(item => (
+          <ItemCard item={item} key={item.id} isClipContent />
+        ))}
+        <ItemsNavigator />
+      </FlexView>
+    </Pressable>
   );
 }
 
