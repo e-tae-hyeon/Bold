be8654983705322aecf19d12460ca9a515c88d63
LTH <johtta888@naver.com>
2023-08-10 22:29:45 +0900

    Feat: 추가 splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from 'navigations/RootStack';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
+
   return (
     <NavigationContainer>
       <RootStack />
